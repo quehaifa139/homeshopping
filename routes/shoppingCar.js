50e151abf990e56535c6e1de22252cafac393282
quehaifa139 <3230334856@qq.com>
2021-05-30 19:57:01 +0800

    new

--- routes/shoppingCar.js
+++ routes/shoppingCar.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//引入数据库
+var connection = require('./user');
+
 router.get('/', function(req, res, next) {
-    res.render('shoppingCar', { title: 'Express' });
-  });
+ var sql = 'select * from shoppingCar';
+   connection.query(sql,(err,result) =>{
+       console.log(err);
+    res.render('shoppingCar', { title: 'Express',data:result });
+   })
+});
 
 module.exports = router;
